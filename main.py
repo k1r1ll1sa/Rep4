3f53689beb4398b66792ec0ee74a094a90fa9d32
kirill <k1r1ll1sa@yandex.ru>
2023-12-21 10:56:10 +0700

    Исправление ошибок pycodesyle

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, \
+     QLineEdit, QPushButton
+
 
 def lighten(color: str, percent: int) -> str:
     # Преобразование строки в строку без символа '#'
     color = color[1:]
-
     # Разделение цвета на составляющие
     r = int(color[0:2], 16)
     g = int(color[2:4], 16)
     b = int(color[4:6], 16)
-
     # Расчет нового значения для каждой составляющей
     r += int((255 - r) * (percent / 100))
     g += int((255 - g) * (percent / 100))
     b += int((255 - b) * (percent / 100))
-
     # Преобразование новых значений в шестнадцатеричную строку
     new_color = f"#{r:02X}{g:02X}{b:02X}"
-
     return new_color
 
 
 def darken(color: str, percent: int) -> str:
     # Преобразование строки в без символа '#'
     color = color[1:]
-
     # Разделение цвета на составляющие
     r = int(color[0:2], 16)
     g = int(color[2:4], 16)
     b = int(color[4:6], 16)
-
     # Расчет нового значения для каждой составляющей
     r -= int(r * (percent / 100))
     g -= int(g * (percent / 100))
     b -= int(b * (percent / 100))
-
     # Преобразование новых значений в шестнадцатеричную строку
     new_color = f"#{r:02X}{g:02X}{b:02X}"
-
     return new_color
 
 
@@ -75,7 +69,7 @@ class ColorModifier(QWidget):
         percent = int(self.input_percent.text())
         new_color = darken(color, percent)
         self.input_color.setText(new_color)
-        
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
