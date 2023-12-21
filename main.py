099c9f410f27fb437df81562c1a44c53173d04db
kirill <k1r1ll1sa@yandex.ru>
2023-12-21 10:47:52 +0700

    Функция Darken

--- main.py
+++ main.py
@@ -21,6 +21,26 @@ def lighten(color: str, percent: int) -> str:
     return new_color
 
 
+def darken(color: str, percent: int) -> str:
+    # Преобразование строки в без символа '#'
+    color = color[1:]
+
+    # Разделение цвета на составляющие
+    r = int(color[0:2], 16)
+    g = int(color[2:4], 16)
+    b = int(color[4:6], 16)
+
+    # Расчет нового значения для каждой составляющей
+    r -= int(r * (percent / 100))
+    g -= int(g * (percent / 100))
+    b -= int(b * (percent / 100))
+
+    # Преобразование новых значений в шестнадцатеричную строку
+    new_color = f"#{r:02X}{g:02X}{b:02X}"
+
+    return new_color
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = ColorModifier()
